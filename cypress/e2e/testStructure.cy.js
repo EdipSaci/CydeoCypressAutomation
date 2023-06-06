18464d2815bb5af2d32bd2eeb2a893a4920d3f91
Edip SACİ <saciedip@gmail.com>
2023-06-06 14:27:23 +0300

    locator task

--- cypress/e2e/testStructure.cy.js
+++ cypress/e2e/testStructure.cy.js
@@ -17,8 +17,19 @@ describe('Context: My first tests', ()=>{
     afterEach(()=>{
         // similar to afterMethod in TestNG 
     })
-    it('Opening a we app',() =>{
+    it.skip('Opening a web app',() =>{
         cy.visit('/registration_form');
-
+    })
+    it('Test 2', ()=>{
+        expect(false).to.equals(false);
+    })
+    it('Test 3', ()=>{
+        expect(false).not.to.equal(true);
+    })
+    it('Test 4', () =>{
+        expect(5).to.equal(5);
+    })
+    it('Test 5', () =>{
+        expect(true).to.equal('5'==5);
     })
 })
