9dd684614056f81a2812f231ef548c80ea645752
Edip SACİ <saciedip@gmail.com>
2023-06-16 14:11:05 +0300

    iterate elements

--- cypress/e2e/buttons.cy.js
+++ cypress/e2e/buttons.cy.js
@@ -19,6 +19,25 @@ describe('Context: My first tests', ()=>{
             //assert the text
             cy.contains('Clicked on button three!').should('be.visible'); 
         })
+
+        // you got all buttons with tagName ; each method is creating me a loop
+        cy.get('button').each((item, index, list) =>{
+            //assert length of the list, verify number of buttons
+            expect(list).to.have.length(6);
+            expect(item).to.have.attr("onclick");
+
+        })
+
+        //I will get all buttons like previous approach, get onyl the item then check for text for each item
+        //if it is equal to Button 4, then click on it 
+        cy.get('button').each((item) =>{
+            if(item.text() == "Button 4"){
+                cy.log(item.text());  // this command write the text at the test console
+                // item.click();  you can not use cypress click func on jQuery element
+                cy.wrap(item).click(); 
+                cy.contains('Clicked on button four!').should('be.visible');
+            }
+        })
     })
     
 })
